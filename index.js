24304a058ffc05ca9f9be9bab5b7645fa1c7eeeb
Turner Milan <turnermilan@gmail.com>
2022-09-06 17:56:44 -0400

    initial setup and a few starting items

---
+++ index.js
@@ -0,0 +1,44 @@
+// display
+const display = document.getElementById('display')
+// operator buttons
+const equalsButton = document.getElementById('equals')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const multiplyButton = document.getElementById('times')
+const divideButton = document.getElementById('by')
+// number buttons
+const zeroButton = document.getElementById('zero')
+const oneButton = document.getElementById('one')
+const twoButton = document.getElementById('two')
+const threeButton = document.getElementById('three')
+const fourButton = document.getElementById('four')
+const fiveButton = document.getElementById('five')
+const sixButton = document.getElementById('six')
+const sevenButton = document.getElementById('seven')
+const eightButton = document.getElementById('eight')
+const nineButton = document.getElementById('nine')
+
+const selectOperation = () => {
+    
+}
+
+const add = (x,y) => {
+    return x+y
+}
+
+const subtract = (x,y) => {
+    return x-y
+}
+
+const multiply = (x,y) => {
+    return x*y
+}
+
+const divide = (x,y) => {
+    return x/y
+}
+
+const operate = () => {
+    
+}
+
