1cc412ac8274207e84e9b00ed6f031f26d135c79
Turner Milan <turnermilan@gmail.com>
2022-09-07 17:59:09 -0400

    started JS functions

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 // DISPLAY
 const display = document.getElementById('display')
+const currNum = document.querySelector('[data-curr-num]')
+const prevNum = document.querySelector('[data-prev-num]')
 // OPERATOR BUTTONS
-
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('[data-equals]')
+const backspaceButton = document.querySelector('[data-backspace]')
+const clearButton = document.querySelector('[data-clear]')
 // NUMBER BUTTONS
-
+const numButtons = document.querySelectorAll('[data-num]')
 // OPERATORS
 const add = (x,y) => x+y
 const subtract = (x,y) => x-y
 const multiply = (x,y) => x*y
 const divide = (x,y) => x/y
 
-let total
-
-const selectOperation = () => {
-    
+class Calculator {
+    constructor(prevNum, currNum){
+        this.prevNum = prevNum
+        this.currNum = currNum
+        // have to reset the inputs
+        this.clearAll()
+    }
 }
 
-
-const operate = (opp, num1, num2) => {
-    
+function clearAll() {
+    this.currOperator = ''
+    this.prevOperator = ''
+    this.operation = undefined
 }
+
+const calculator = new Calculator(prevNum, currNum)
+
+backspace = () => {
+
+}
+
 
