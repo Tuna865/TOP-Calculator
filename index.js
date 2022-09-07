48a0d73352e1d41dd6dc763ad4c698ede8518043
Turner Milan <turnermilan@gmail.com>
2022-09-06 21:54:04 -0400

    completed calculator front end

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// display
+// DISPLAY
 const display = document.getElementById('display')
-// operator buttons
+// OPERATOR BUTTONS
 const equalsButton = document.getElementById('equals')
 const plusButton = document.getElementById('plus')
 const minusButton = document.getElementById('minus')
 const multiplyButton = document.getElementById('times')
-const divideButton = document.getElementById('by')
-// number buttons
+const divideButton = document.getElementById('divided')
+// NUMBER BUTTONS
 const zeroButton = document.getElementById('zero')
 const oneButton = document.getElementById('one')
 const twoButton = document.getElementById('two')
@@ -17,28 +17,20 @@ const sixButton = document.getElementById('six')
 const sevenButton = document.getElementById('seven')
 const eightButton = document.getElementById('eight')
 const nineButton = document.getElementById('nine')
+// OPERATORS
+const add = (x,y) => x+y
+const subtract = (x,y) => x-y
+const multiply = (x,y) => x*y
+const divide = (x,y) => x/y
+
+let total
 
 const selectOperation = () => {
     
 }
 
-const add = (x,y) => {
-    return x+y
-}
 
-const subtract = (x,y) => {
-    return x-y
-}
-
-const multiply = (x,y) => {
-    return x*y
-}
-
-const divide = (x,y) => {
-    return x/y
-}
-
-const operate = () => {
+const operate = (opp, num1, num2) => {
     
 }
 
