b3529a142f704ba742c87abb49891a6be16f68d7
Turner Milan <turnermilan@gmail.com>
2022-09-06 22:43:44 -0400

    more small styling

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 // DISPLAY
 const display = document.getElementById('display')
 // OPERATOR BUTTONS
-const equalsButton = document.getElementById('equals')
-const plusButton = document.getElementById('plus')
-const minusButton = document.getElementById('minus')
-const multiplyButton = document.getElementById('times')
-const divideButton = document.getElementById('divided')
+
 // NUMBER BUTTONS
-const zeroButton = document.getElementById('zero')
-const oneButton = document.getElementById('one')
-const twoButton = document.getElementById('two')
-const threeButton = document.getElementById('three')
-const fourButton = document.getElementById('four')
-const fiveButton = document.getElementById('five')
-const sixButton = document.getElementById('six')
-const sevenButton = document.getElementById('seven')
-const eightButton = document.getElementById('eight')
-const nineButton = document.getElementById('nine')
+
 // OPERATORS
 const add = (x,y) => x+y
 const subtract = (x,y) => x-y
