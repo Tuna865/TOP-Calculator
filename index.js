79806af4ac560602709e8c17c330fd11a3a350bb
Turner Milan <turnermilan@gmail.com>
2022-09-07 20:37:17 -0400

    more styling and js functionality

--- index.js
+++ index.js
@@ -14,15 +14,14 @@ const add = (x,y) => x+y
 const subtract = (x,y) => x-y
 const multiply = (x,y) => x*y
 const divide = (x,y) => x/y
-
 class Calculator {
     constructor(prevNum, currNum){
         this.prevNum = prevNum
         this.currNum = currNum
-        // have to reset the inputs
         this.clearAll()
     }
 }
+const calculator = new Calculator(prevNum, currNum)
 
 function clearAll() {
     this.currOperator = ''
@@ -30,10 +29,92 @@ function clearAll() {
     this.operation = undefined
 }
 
-const calculator = new Calculator(prevNum, currNum)
 
-backspace = () => {
-
+function backspace() {
+    this.currOperator = this.currOperator.toString().slice(0, -1) 
 }
 
+function updateDisplay(){
+    if(this.operation != null){
+        this.prevNum.innerText = `${this.getDisplayNum(this.prevOperator)} ${this.operation}`
+    }
+}
 
+function evaluate(){
+    let result
+    const previous = parseFloat(this.prevOperator)
+    const current = parseFloat(this.currOperator)
+    if(isNaN(previous) || isNaN(current)){
+        return
+    }
+    switch (this.operation){
+        // these can be done more simply but this is cool 
+        case '+':
+            result = add(previous,current)
+            break
+        case '-':
+            result = subtract(previous,current)
+            break
+        case 'x':
+            result = multiply(previous,current)
+            break
+        case '÷':
+            result = divide(previous,current)
+            break
+        default: 
+            return
+    }
+    this.currOperator = result
+    this.operation = undefined
+    this.prevOperator = ''
+}
+
+function appendNumber(num) {
+    // prevents duplicate periods/decimal points
+    if(num === '.' && this.currOperator.includes('.')){
+        return
+    }
+    this.currOperator = this.currOperator.toString() + num.toString()
+}
+
+function chooseOperation(operation){
+    if(this.currOperator === ''){
+        return
+    }
+    if(this.prevOperator === ''){
+        this.evaluate()
+    }
+    this.operation = operation
+    this.prevOperator = this.currOperator
+    this.currOperator = ''
+}
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // corresponding number comes from the html in the button itself
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+// same as number buttons 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.evaluate()
+    calculator.updateDisplay()
+})
+
+clearButton.addEventListener('click', () =>{
+    calculator.clearAll()
+    calculator.updateDisplay()
+})
+backspaceButton.addEventListener('click', () =>{
+    calculator.backspace()
+    calculator.updateDisplay()
+})
